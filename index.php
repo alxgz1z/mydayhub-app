e55151023566e943a33e378bf939fa18db053497
AlexG <jalexg@icloud.com>
2025-08-01 23:33:59 -0400

    view containers added

--- index.php
+++ index.php
@@ -30,6 +30,7 @@ require_once __DIR__ . '/includes/config.php';
                     <button class="view-tab active" data-view="tasks">Tasks</button>
                     <button class="view-tab" data-view="journal">Journal</button>
                     <button class="view-tab" data-view="outlines">Outlines</button>
+                    <button class="view-tab" data-view="meetings">Meetings</button>
                 </nav>
             </div>
             <div class="header-right">
@@ -37,7 +38,18 @@ require_once __DIR__ . '/includes/config.php';
         </header>
 
         <main id="main-content">
-            </main>
+            <div id="tasks-view-container" class="view-container active">
+                </div>
+
+            <div id="journal-view-container" class="view-container" style="display: none;">
+                </div>
+
+            <div id="outlines-view-container" class="view-container" style="display: none;">
+                </div>
+
+            <div id="meetings-view-container" class="view-container" style="display: none;">
+                </div>
+        </main>
 
     </div>
 
